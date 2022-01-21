6065100fc1b425ce1c05bc9d7b5b9e8e6c896192
arshivam <shivam.armarkar007@gmail.com>
2022-01-21 19:30:41 +0530

    ADDED : program for computing Largest Number

--- LargestNum.java
+++ LargestNum.java
@@ -3,19 +3,20 @@ import java.util.*;
 
 public class LargestNum {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter Three Numbers : ");
-		int num1 = sc.nextInt();
-		int num2 = sc.nextInt();
-		int num3 = sc.nextInt();
-		if (num1>num2 && num1>num3) {
-			System.out.println("The largest Number is :"+num1);
-		}
-		else if (num2>num1 && num2>num3) {
-			System.out.println("The largest Number is :"+num2);
-		}
-		else {
-			System.out.println("The largest Number is :"+num3);
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Enter Three Numbers : ");
+			int num1 = sc.nextInt();
+			int num2 = sc.nextInt();
+			int num3 = sc.nextInt();
+			if (num1>num2 && num1>num3) {
+				System.out.println("The largest Number is :"+num1);
+			}
+			else if (num2>num1 && num2>num3) {
+				System.out.println("The largest Number is :"+num2);
+			}
+			else {
+				System.out.println("The largest Number is :"+num3);
+			}
 		}
 	}
 }
