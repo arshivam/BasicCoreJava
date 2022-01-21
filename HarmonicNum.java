49a0010639ba017f5a7aeb6719032e04c3576e9c
arshivam <shivam.armarkar007@gmail.com>
2022-01-21 17:50:11 +0530

    ADDED : harmonic Number program

---
+++ HarmonicNum.java
@@ -0,0 +1,16 @@
+package com.basiccorejava;
+import java.util.*;
+
+public class HarmonicNum {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the num :");
+		int n = sc.nextInt();
+		double result = 0;
+		for (double i = 1;i<n;i++) {
+			double reciprocal = 1 / i;
+			result = result + reciprocal;
+		}
+			System.out.println("Result : "+result);
+	}
+}
--- LeapYear.java
+++ LeapYear.java
@@ -4,10 +4,10 @@ import java.util.*;
 public class LeapYear {
 	public static void main(String[] args) {
 		System.out.println("WELCOME TO LEAP YEAR USE CASE");
-		Scanner s = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		int year;
 		System.out.println("Enter year");
-		year = s.nextInt();
+		year = sc.nextInt();
 		if (year>999 && year<10000) {
 			if (year % 4 ==0 && year % 100 ==0 && year % 400 ==0) {
 				System.out.println("SPECIFIED YEAR IS LEAP YEAR");
